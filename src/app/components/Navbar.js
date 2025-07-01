4840889f571e7c4007425e29799815d362ff46fb
Somyaranjan Sahoo <soumyaranjansahoo@Somyas-MacBook-Air.local>
2025-07-01 22:25:45 +0530

    changed reupria to repuies

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -146,7 +146,7 @@ const Navbar = () => {
               : "text-white"
           }`}
         >
-          Rupeia
+          Rupeias
         </p>
       </div>
       <div className="flex flex-row items-center gap-2">
