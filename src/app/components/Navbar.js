a57772dc76f64cfa17d6d74d2bf52ce8f8031d92
Somyaranjan Sahoo <soumyaranjansahoo@Somyas-Air.ga-430.nettantra.com>
2025-07-05 17:53:24 +0530

    Changed the PWA name

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -146,7 +146,7 @@ const Navbar = () => {
               : "text-white"
           }`}
         >
-          Rupeias
+          Rupeia
         </p>
       </div>
       <div className="flex flex-row items-center gap-2">
